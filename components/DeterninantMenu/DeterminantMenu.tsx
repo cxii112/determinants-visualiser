8072fe240fbfdf73d64411df785cc1b3695439fe
cxii112 <lyakhovoy.d@ya.ru>
2022-07-22 02:55:58 +0500

    auto render

--- components/DeterninantMenu/DeterminantMenu.tsx
+++ components/DeterninantMenu/DeterminantMenu.tsx
@@ -1,19 +1,25 @@
 import DeterminantCell from "./DeterminantCell";
-import ListOptionPicker from "../ListOptionPicker";
+import ListOptionPicker, {options} from "../ListOptionPicker";
+import {UpdateMaskContext} from "../../context/UpdateMaskContext";
+import {useContext} from "react";
 
 export default function DeterminantMenu() {
-  let cells = [];
-  for (let i =0; i<9;i++)
-  {
-    cells.push(<DeterminantCell className={"w-fit h-fit"} key={i}>
-      <ListOptionPicker/>
-    </DeterminantCell>)
-  }
-  return <>
-    <div className="aspect-square border-l-2 border-r-2 border-white min-w-fit flex justify-center align-middle py-1">
-      <div className="grid grid-cols-3 gap-x-4 gap-y-2 px-2">
-        {cells}
-      </div>
-    </div>
-  </>;
+    let cells = [];
+    const updateMaskContext = useContext(UpdateMaskContext);
+    for (let i = 0; i < 9; i++) {
+        cells.push(<DeterminantCell className={"w-fit h-fit"} key={i}>
+            <ListOptionPicker index={i}
+                              options={options}
+                              callback={updateMaskContext}
+            />
+        </DeterminantCell>);
+    }
+    return <>
+        <div
+            className="aspect-square border-l-2 border-r-2 border-white min-w-fit flex justify-center align-middle py-1">
+            <div className="grid grid-cols-3 gap-x-4 gap-y-2 px-2">
+                {cells}
+            </div>
+        </div>
+    </>;
 }
