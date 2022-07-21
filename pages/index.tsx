7552f90477e4ac6557a5a0b7e0c5c56f13219daf
cxii112 <lyakhovoy.d@ya.ru>
2022-07-22 02:56:16 +0500

    data manipulations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,44 @@
-import type { NextPage } from 'next';
-import * as data from "../public/GamesData.json";
-import {useEffect} from "react";
+import type {NextPage} from "next";
+import data from "../public/GamesData.json";
+import {useEffect, useState} from "react";
 import DeterminantMenu from "../components/DeterninantMenu/DeterminantMenu";
+import {UpdateMaskContext} from "../context/UpdateMaskContext";
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    return () => {
-      console.log(data.length);
-    };
-  }, []);
+    const determinantsData = (data as {Key:string, Det:number}[]);
+    const [mask, setMask] = useState(".........");
+    const [filtered, setFiltered] = useState(determinantsData);
 
-  return (
-    <div>
-      <div className="fixed inset-0 flex items-center justify-center bg-zinc-900">
-        <DeterminantMenu/>
-      </div>
-    </div>
-  )
-}
+    function updateMask(key: number, value: number | undefined) {
+        setMask(prev => {
+            const replacement = value !== undefined ? value.toString() : ".";
+            return prev.substring(0, key) + replacement + prev.substring(key + 1);
+        });
+    }
 
-export default Home
+    function filterDataByMask() {
+        let newFiltered: { Key: string; Det: number; }[] = [];
+        determinantsData.map((value) => {
+            if (value.Key.match(new RegExp(mask))) newFiltered.push(value);
+        });
+        setFiltered(newFiltered);
+    }
+
+    useEffect(() => {
+        console.debug(mask);
+        filterDataByMask();
+    }, [mask]);
+
+
+    return (
+        <div>
+            <div className="fixed inset-0 flex items-center justify-center bg-zinc-900">
+                <UpdateMaskContext.Provider value={updateMask}>
+                    <DeterminantMenu/>
+                </UpdateMaskContext.Provider>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
